d25ca427476f98426274a090f0832ae499fabd02
Willian Takanage <80928807+takanage@users.noreply.github.com>
2021-04-08 07:06:22 -0300

    Atualizando script
    Executando o exercício

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-var inicio = document.getElementById('inicio')
-var fim = document.getElementById('fim')
-var contar = document.getElementById('contar')
-var res = document.getElementById('res')
+function contar(){
 
-while (inicio <= fim) {
+    var ini = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
+    var res = document.getElementById('res')
     
-}
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('[ERRO] Faltam dados')
+    }else {
+        res.innerHTML = 'Contando: '
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        
+        for(var c = i; c <= f; c += p)
+        res.innerHTML +=`${c}`
+    }
+  
