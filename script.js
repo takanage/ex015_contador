bb19af613ae3b3e31cd49266f3635b55f072762a
Willian Takanage <80928807+takanage@users.noreply.github.com>
2021-04-08 06:25:27 -0300

    Iniciando exercício
    - Criado o repositório

---
+++ script.js
@@ -0,0 +1,8 @@
+var inicio = document.getElementById('inicio')
+var fim = document.getElementById('fim')
+var contar = document.getElementById('contar')
+var res = document.getElementById('res')
+
+while (inicio <= fim) {
+    
+}
